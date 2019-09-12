240992f6ff441d4001138700234d0dbff07af2f9
deibyocampo <deiby13_96@hotmail.com>
2019-09-12 17:20:31 -0500

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,9 @@
 def print_last_digit(number):
     last = 0
     if number < 0:
-        last = number % -10 
+        last = number % -10
         last *= -1
-    if number >= 0:
+    else:
         last = number % 10
     print("{:d}".format(last), end="")
     return last
