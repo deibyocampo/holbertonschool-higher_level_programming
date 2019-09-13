8d06c777ed6cceae06d40601189d21b4b78af7b3
deibyocampo <deiby13_96@hotmail.com>
2019-09-12 23:25:05 -0500

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    if argc == 0:
+        print("{:d} arguments.".format(argc - 1))
+    elif argc == 2:
+        print("{} argument:".format(argc - 1))
+        print("{}: {}".format(argc - 1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(argc - 1))
+
+        for index, date in enumerate(sys.argv):
+            if index != 0:
+                print("{}: {}".format(index, date))
+
