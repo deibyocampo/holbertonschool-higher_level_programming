dba7d33f7a8316755941c259fe82e0de203bfef3
deibyocampo <deiby13_96@hotmail.com>
2019-09-13 21:29:02 -0500

    alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(ord('z'), ord('a') -1, -1):
     if n % 2 == 0:
-        print("{}".format(chr(n-32)), end='')
-    else:
-        print("{}".format(chr(n)), end='')
+        print("{}".format(chr(n - 32)), end='')
+#    else:
+#        print("{}".format(chr(n)), end='')
