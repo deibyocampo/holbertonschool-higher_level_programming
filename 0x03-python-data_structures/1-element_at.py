88f880d5049f312e7357d5cf74f6ec7f9f86bb11
deibyocampo <deiby13_96@hotmail.com>
2019-09-17 20:24:42 -0500

    access to an element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     for i in my_list:
         if i == idx:
-            return (my_list[i])
+            return (my_list[idx])
         if idx < 0:
             return ("None")
         if idx > len(my_list):
