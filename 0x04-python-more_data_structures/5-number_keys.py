7b5e2db627a2108faba29ae99d1ab2ee008de216
deibyocampo <deiby13_96@hotmail.com>
2019-09-19 23:35:56 -0500

    number keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!usr/bin/python3
 def number_keys(a_dictionary):
-    my_list = list(a_dictionary.values())
-    count = 0
-    for i in my_list:
-        count += 1
-    return count
+    return len(a_dictionary.keys())
