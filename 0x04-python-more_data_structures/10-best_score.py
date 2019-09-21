162b783441ecc4d64f4c2a609acf996464298a9b
deibyocampo <deiby13_96@hotmail.com>
2019-09-21 18:45:20 -0500

    best score person

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if  a_dictionary is None:
+    if a_dictionary is None:
         return None
     new_dic = list(a_dictionary.values())
     for key, val in a_dictionary.items():
