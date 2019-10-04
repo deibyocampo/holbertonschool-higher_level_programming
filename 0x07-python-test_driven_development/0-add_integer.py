cc13c5109ec66ea0a5ab62f33a19cdf4e602d760
deibyocampo <deiby13_96@hotmail.com>
2019-10-04 18:52:04 -0500

    integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+"""An addition module
+add_integer adds two integers together
+
+"""
+
 
 def add_integer(a, b=98):
-    """ a and b must integers or floats,
-        otherwise raise error diferent date
+    """ Return a + b
+    Args: a and b
     """
-    if isinstance(a, float) or isinstance(b, float):
+    if isinstance(a, float):
         a = int(a)
+    if isinstance(b, float):
         b = int(b)
     if not isinstance(a, int):
         raise TypeError("a must be an integer")
     if not isinstance(b, int):
         raise TypeError("b must be an integer")
-    return a + b
+    return (int(a) + int(b))
