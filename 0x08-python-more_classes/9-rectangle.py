48474944d9b740ab1c69cb71d9dc271ac78283c1
deibyocampo <deiby13_96@hotmail.com>
2019-10-18 23:03:24 -0500

    change methods

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
@@ -46,16 +46,16 @@ class Rectangle:
             return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        if self.__height == 0 or self.__width == 0:
+        if self.height == 0 or self.__width == 0:
             return ("")
         else:
             for a in range(self.__height):
                 for b in range(self.__height - 1):
                     print(str(self.print_symbol) * self.__width)
-                return (str(self.print_symbol) * self.__width)
+                return (str(self.print_symbol) * self.width)
 
     def __repr__(self):
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
