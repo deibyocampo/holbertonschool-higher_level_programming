04275ee3bacb7f74411c0021a50582443782e559
deibyocampo <deiby13_96@hotmail.com>
2019-10-19 20:50:24 -0500

    only diferents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_combo = sorted(set_1.union(set_2))
-    common_val = set_1 - (set(set_1) - set(set_2))
-    for i in set_combo:
-        for x in common_val:
-            if x == i:
-                set_combo.remove(i)
-    return set_combo
+    return set_1 ^ set_2
