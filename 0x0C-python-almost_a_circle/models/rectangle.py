9681163c9bb507e5d7fc21cdbb06630c17090b45
deibyocampo <deiby13_96@hotmail.com>
2019-10-21 22:49:39 -0500

    validation attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,6 +16,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, width):
+        self.integer_validator("width", width)
         self.__width = width
 
     @property
@@ -24,6 +25,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
+        self.integer_validator("height", height)
         self.__height = height
 
     @property
@@ -32,6 +34,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        self.integer_validator("x", x)
         self.__x = x
 
     @property
@@ -40,4 +43,15 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        self.integer_validator("y", y)
         self.__y = y
+
+    def integer_validator(self, name, value):
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if name == "width" or name == "height":
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(name))
+        if name == "x" or name == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(name))
