c12823022f7a50210ed152363cb6712b00a014c7
deibyocampo <deiby13_96@hotmail.com>
2019-10-22 23:04:54 -0500

    comments the class base and functions

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+""" create Base class"""
+
+
 class Base:
+    """ Base class """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Assign the public instance atribute id """
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
