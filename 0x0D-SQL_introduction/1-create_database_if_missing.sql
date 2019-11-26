ff7189e32417f420f6a3fc8b0c880450864cdab1
deibyocampo <deiby13_96@hotmail.com>
2019-11-26 00:00:36 -0500

    sql introduction

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- create the database hbtn_0c_0 MySQL server
+-- if the database hbtn_0c_0 already exists, the script
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- deletes the database hbtn_0c_0 in your MySQL server
+-- if the database hbtn_0c_0 doesnt exits
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
