20b5819ed5e9468d3dca914bf916bc3030c59c4c
deibyocampo <deiby13_96@hotmail.com>
2019-11-27 23:00:13 -0500

    adding condition where the name is no null

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
 -- list all records of the table second_table of the database
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL ORDER BY score DESC;
