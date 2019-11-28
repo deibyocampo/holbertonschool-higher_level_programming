929617f45b0e41428c413f55d15115bded95b877
deibyocampo <deiby13_96@hotmail.com>
2019-11-27 23:33:06 -0500

    cities by state join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,8 @@
--- creates table and databases into MySQL
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- list all shows contained in hbtn_0d_tvshows that at least one gente linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
--- creates table and databases into MySQL
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- list all cities contained the database hbtn_0d_usa
+
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
