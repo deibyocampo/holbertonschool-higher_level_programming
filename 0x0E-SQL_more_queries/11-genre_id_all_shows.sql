e3cc45da260f570b516ed7127cdf0b643fd85aef
deibyocampo <deiby13_96@hotmail.com>
2019-11-29 20:28:54 -0500

    genre id all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,7 @@
--- creates table and databases into MySQL
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- database sump of hbtn_od_tvshows to your MySQL server
+
+SElECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,8 @@
--- creates table and databases into MySQL
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- database sump of hbtn_od_tvshows to your MySQL server
+
+SElECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
