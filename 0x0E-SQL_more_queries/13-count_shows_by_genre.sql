2deba60ba77b6fa0d6093ba349d37de727289f69
deibyocampo <deiby13_96@hotmail.com>
2019-11-29 20:56:03 -0500

    my genres and count show be genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,9 @@
--- creates table and databases into MySQL
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- database dump from hbtn_0d_tvshows to your MySQL server
+
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+FROM tv_genres
+LEFT OUTER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id IS NOT NULL
+GROUP BY genre
+ORDER BY number_shows DESC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,10 @@
--- creates table and databases into MySQL
-SELECT
-FROM
-ORDER BY
-IGNORE ALL;
+-- database dump from hbtn_0d_tvshows to your MySQL server
+
+SELECT tv_genres.name
+FROM tv_genres
+LEFT OUTER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+LEFT OUTER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
