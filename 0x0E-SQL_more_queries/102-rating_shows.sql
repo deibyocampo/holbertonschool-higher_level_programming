b54bc16c82d1e0f6dd25f2c22df8243ca83b0a3c
deibyocampo <deiby13_96@hotmail.com>
2019-12-06 16:57:45 -0500

    rating shows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,5 +3,5 @@
 SELECT tv_genres.name, Sum(tv_show_ratings.rate) AS rating
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_show_ratings
-ON tv_shows_ratings.show_id = tv_show_genres.show_id
+ON tv_show_ratings.show_id = tv_show_genres.show_id
 GROUP BY tv_genres.name ORDER BY rating DESC;
