8b817a32764d41128615213b89f12adb16828644
deibyocampo <deiby13_96@hotmail.com>
2019-12-06 02:04:38 -0500

    count shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,8 @@
 -- database dump from hbtn_0d_tvshows to your MySQL server
 
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_genres
-LEFT OUTER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id IS NOT NULL
-GROUP BY genre
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.genre_id
+GROUP BY genre HAVING number_shows > 0
 ORDER BY number_shows DESC
