528e1992c1a9abcd3ec5c4a10990c9e71c24541f
deibyocampo <deiby13_96@hotmail.com>
2019-12-20 18:44:56 -0500

    model state insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" List all state ibjects from the database hbtn_0e_6_usa """
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    session = sessionmaker(bind=engine)
+    sesh = session()
+
+    new_obj = State(name="louisiana")
+    sesh.add(new_obj)
+    sesh.commit()
+
+    print(new_obj.id)
+    sesh.close()
