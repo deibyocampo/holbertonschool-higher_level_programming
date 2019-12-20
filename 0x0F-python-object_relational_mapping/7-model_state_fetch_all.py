7984969598735f53b2f2f8a5137793f066a2ad12
deibyocampo <deiby13_96@hotmail.com>
2019-12-20 18:14:14 -0500

    model state fetch all

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" List all state ibjects from the database hbtn_0e_6_usa """
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    session = sessionmaker(bind=engine)
+    sesh = session()
+
+    for state in sesh.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    sesh.close()
