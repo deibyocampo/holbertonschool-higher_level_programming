c73c7456318ae29edb85a825632ab087ee28b57a
deibyocampo <deiby13_96@hotmail.com>
2020-01-13 23:43:31 -0500

    hbtn status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    """
+    python
+    """
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
