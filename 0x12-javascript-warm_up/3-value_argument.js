4da0b83953827339027c3ec4aa3882c3d8ff7bf7
deibyocampo <deiby13_96@hotmail.com>
2020-02-04 16:47:52 -0500

    value arguments to string

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const index = process.argv.slice(2);
+
 if (index < 1) {
   console.log('No argument');
 }
-if (index !== 1) {
-  console.log(`${index}`);
+else {
+  console.log(index.toString());
 }
