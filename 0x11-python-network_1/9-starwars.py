822be7788b8d1802ea4f4324ab8ca3099e5c347f
deibyocampo <deiby13_96@hotmail.com>
2020-02-08 23:51:03 -0500

    star wars api

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+searches star wars API
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    search = {'search': sys.argv[1]}
+    req = requests.get('https://swapi.co/api/people', params=search)
+    results = req.json()
+    count = results.get('count')
+    print("Number of results: {}".format(count))
+    for i in results.get('results'):
+        print(i.get('name'))
