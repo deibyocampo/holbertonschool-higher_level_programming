f9af470300f0277a484316ba2a57e68172e4eb32
deibyocampo <deiby13_96@hotmail.com>
2020-02-08 20:00:30 -0500

    change the conditional if

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,13 +4,18 @@
 
 def find_peak(list_of_integers):
     """ Function to find peak in array """
+    peak = 0
     if len(list_of_integers) == 0:
         return None
 
-    for i in range(len(list_of_integers)):
-        if i != len(list_of_integers) - 1:
-            if list_of_integers[i] > list_of_integers[i + 1]:
-                return list_of_integers[i]
+    for i in range(len(list_of_integers) - 1):
+        next_int = i + 1
+        if list_of_integers[next_int] >= list_of_integers[i]:
+            if len(list_of_integers) <= 3\
+               and list_of_integers[next_int] >= list_of_integers[i]:
+                peak = list_of_integers[next_int]
+            if i != 0 and next_int != len(list_of_integers) - 1:
+                peak = list_of_integers[next_int]
         else:
-            if list_of_integers[i] > list_of_integers[i - 1]:
-                return list_of_integers[i]
+            continue
+    return peak
