f0f8b691a709539f0bd83be8a15dae4a6562c368
deibyocampo <deiby13_96@hotmail.com>
2020-02-09 01:10:25 -0500

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Display 10 commits of the repository
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    repo = argv[1]
+    owner = argv[1]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+
+    r = requests.get(url)
+    commits = r.json()
+
+    for i in range(10):
+        commit = commits[i]
+        name = ((commit['commit'])['author'])['name']
+        sha = commit['sha']
+        print("{}: {}".format(sha, name))
