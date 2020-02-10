372e86b618bc6236d6c91b2198993d1fa1e3c7b7
deibyocampo <deiby13_96@hotmail.com>
2020-02-10 00:41:52 -0500

    star wars api #1

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+searches star wars API with string
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    search = argv[1]
+    url = url_first = 'https://swapi.co/api/people'
+
+    while url is not None:
+        req = requests.get(url, params={'search': search})
+        results = req.json()
+
+        if url == url_first:
+            print("Number of results: {}".format(results['count']))
+        for i in results['results']:
+            print(i['name'])
+        url = results['next']
