b0dd9a33ae355c3248cf303b41130862628f47bb
deibyocampo <deiby13_96@hotmail.com>
2020-02-11 22:58:26 -0500

    statuscode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// display the status code of a GET request
+
+const request = require('request');
+const argv = process.argv;
+const url = argv[2];
+
+request.get(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:' + response.statusCode);
+  }
+});
