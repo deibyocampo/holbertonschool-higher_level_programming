dde2c59c4e8323cd4affbaba4968109983ddcb4b
deibyocampo <deiby13_96@hotmail.com>
2020-02-12 21:46:47 -0500

    ocurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
-// in process
+// Returns the number of ocurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return (count);
+};
