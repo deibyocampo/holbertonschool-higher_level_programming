deec96627c923b85f3c6d4705a31a1f2be1de0e6
deibyocampo <deiby13_96@hotmail.com>
2020-02-13 00:02:56 -0500

    completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,3 +3,21 @@
 
 const request = require('request');
 const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  const tasks = JSON.parse(body);
+  const obj = {};
+  for (const task of tasks) {
+    if (task.completed === true) {
+      if (obj[task.userId] === undefined) {
+        obj[task.userId] = 1;
+      } else {
+        obj[task.userId]++;
+      }
+    }
+  }
+  console.log(obj);
+});
