1931d5a056a4c3346e70b50d37cfbec8cea31eb7
deibyocampo <deiby13_96@hotmail.com>
2020-02-12 22:37:29 -0500

    esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,6 +2,12 @@
 // in process
 
 exports.esrever = function (list) {
-  const reversed = list.reverse();
-  console.log(reversed);
+  let size = list.length - 1;
+  for (let i = 0; i <= size; i++) {
+    const temp = list[size];
+    list[size] = list[i];
+    list[i] = temp;
+    size--;
+  }
+  return (list);
 };
